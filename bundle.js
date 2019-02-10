733f81c95322cdbe340b1d5a35f7f71dee95c48c
Walter Dal Mut <walter.dalmut@gmail.com>
2019-02-10 11:04:14 +0100

    Moved requires on top

--- bundle.js
+++ bundle.js
@@ -12505,21 +12505,19 @@ module.exports = () => {
 },{"../defaults":332}],352:[function(require,module,exports){
 (function (setImmediate){
 ;(function() {
-  const { assoc, prop } = require('ramda')
-
+  const { assoc, prop }   = require('ramda')
   const create_play_board = require('./graphics/play-board')
-  const canvas = create_play_board()
+  const draw              = require('./graphics/display')
+  const reset_game        = require('./game/reset-game')
+  const update_state       = require('./game/apply-state')
 
-  const draw = require('./graphics/display')
+  const canvas         = create_play_board()
   const draw_on_canvas = draw(canvas.getContext('2d'))
-
-  const apply_state = require('./game/apply-state')
-  const reset_game = require('./game/reset-game')
 
   let state = reset_game()
 
   function play() {
-    state = apply_state(state)
+    state = update_state(state)
 
     draw_on_canvas(state)
 
